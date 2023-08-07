14d52fd31d1280ad429abaa25239d446e8de868c
Ibrahim Fathi <himafathy796@gmail.com>
2023-08-07 15:17:02 +0300

    book entity

---
+++ lib/features/home/domain/entities/book_entity.dart
@@ -0,0 +1,16 @@
+class BookEntity {
+  final String image;
+  final String title;
+  final num price;
+  final String authorName;
+  final String rating;
+
+  BookEntity(
+    this.image,
+    this.title,
+    this.price,
+    this.authorName,
+    this.rating,
+  );
+  
+}
