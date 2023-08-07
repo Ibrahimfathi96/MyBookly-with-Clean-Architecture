243691dd2e96cd43ac081df316476c922dd4b327
Ibrahim Fathi <himafathy796@gmail.com>
2023-08-07 16:12:57 +0300

    fetch newest books

--- lib/features/home/domain/use_cases/fetch_newest_books_use_case.dart
+++ lib/features/home/domain/use_cases/fetch_newest_books_use_case.dart
@@ -3,13 +3,14 @@ import 'package:my_bookly_clean_architecture/core/errors/failure.dart';
 import 'package:my_bookly_clean_architecture/core/use_case/no_params_use_case.dart';
 import 'package:my_bookly_clean_architecture/features/home/domain/entities/book_entity.dart';
 import 'package:my_bookly_clean_architecture/features/home/domain/repos/home_repo.dart';
-class FetchFeaturedBooksUseCase extends NoParamsUseCase<List<BookEntity>> {
+
+class FetchNewestBooksUseCase extends NoParamsUseCase<List<BookEntity>> {
   final HomeRepo homeRepo;
 
-  FetchFeaturedBooksUseCase(this.homeRepo);
+  FetchNewestBooksUseCase(this.homeRepo);
 
   @override
   Future<Either<Failure, List<BookEntity>>> execute() async {
-    return await homeRepo.fetchFeaturedBooks();
+    return await homeRepo.fetchNewestBooks();
   }
 }
