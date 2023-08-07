3d27a58242db5f7d9a0901027e4252232179fd9d
Ibrahim Fathi <himafathy796@gmail.com>
2023-08-07 15:17:17 +0300

    home repo

---
+++ lib/features/home/domain/repos/home_repo.dart
@@ -0,0 +1,7 @@
+import 'package:my_bookly_clean_architecture/features/home/domain/entities/book_entity.dart';
+
+abstract class HomeRepo {
+  Future<List<BookEntity>> fetchFeaturedBooks();
+  Future<List<BookEntity>> fetchNewestBooks();
+
+}
