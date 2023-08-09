217101125b3602addef5a860c189dc219a6acffa
Ibrahim Fathi <himafathy796@gmail.comhimafathy796@gmail.com>
2023-08-09 19:37:27 +0300

    home repo implementation

--- lib/features/home/data/repos/home_repo_impl.dart
+++ lib/features/home/data/repos/home_repo_impl.dart
@@ -1,19 +1,40 @@
 import 'package:dartz/dartz.dart';
 import 'package:my_bookly_clean_architecture/core/errors/failure.dart';
+import 'package:my_bookly_clean_architecture/features/home/data/data_sources/home_local_data_source.dart';
+import 'package:my_bookly_clean_architecture/features/home/data/data_sources/home_remote_data_source.dart';
 import 'package:my_bookly_clean_architecture/features/home/domain/entities/book_entity.dart';
 import 'package:my_bookly_clean_architecture/features/home/domain/repos/home_repo.dart';
 
-class HomeRepoImpl extends HomeRepo{
+class HomeRepoImpl extends HomeRepo {
+  final HomeLocalDataSource homeLocalDataSource;
+  final HomeRemoteDataSource homeRemoteDataSource;
+  HomeRepoImpl(
+      {required this.homeLocalDataSource, required this.homeRemoteDataSource});
   @override
-  Future<Either<Failure, List<BookEntity>>> fetchFeaturedBooks() {
-    // TODO: implement fetchFeaturedBooks
-    throw UnimplementedError();
+  Future<Either<Failure, List<BookEntity>>> fetchFeaturedBooks() async {
+    try {
+      var booksList = homeLocalDataSource.fetchFeaturedBooks();
+      if (booksList.isNotEmpty) {
+        return right(booksList);
+      }
+      var books = await homeRemoteDataSource.fetchFeaturedBooks();
+      return right(books);
+    } catch (e) {
+      return left(Failure());
+    }
   }
 
   @override
-  Future<Either<Failure, List<BookEntity>>> fetchNewestBooks() {
-    // TODO: implement fetchNewestBooks
-    throw UnimplementedError();
+  Future<Either<Failure, List<BookEntity>>> fetchNewestBooks()async {
+    try {
+      var booksList = homeLocalDataSource.fetchNewestBooks();
+      if (booksList.isNotEmpty) {
+        return right(booksList);
+      }
+      var books = await homeRemoteDataSource.fetchNewestBooks();
+      return right(books);
+    } catch (e) {
+      return left(Failure());
+    }
   }
-
 }
