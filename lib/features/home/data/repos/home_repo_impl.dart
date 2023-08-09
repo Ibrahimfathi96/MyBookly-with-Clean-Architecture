5e98b90086cd57ef7a8067d8658e92ea54857a54
Ibrahim Fathi <himafathy796@gmail.comhimafathy796@gmail.com>
2023-08-09 19:27:53 +0300

    create home repo impl

---
+++ lib/features/home/data/repos/home_repo_impl.dart
@@ -0,0 +1,19 @@
+import 'package:dartz/dartz.dart';
+import 'package:my_bookly_clean_architecture/core/errors/failure.dart';
+import 'package:my_bookly_clean_architecture/features/home/domain/entities/book_entity.dart';
+import 'package:my_bookly_clean_architecture/features/home/domain/repos/home_repo.dart';
+
+class HomeRepoImpl extends HomeRepo{
+  @override
+  Future<Either<Failure, List<BookEntity>>> fetchFeaturedBooks() {
+    // TODO: implement fetchFeaturedBooks
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<Either<Failure, List<BookEntity>>> fetchNewestBooks() {
+    // TODO: implement fetchNewestBooks
+    throw UnimplementedError();
+  }
+
+}
